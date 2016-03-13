19d6880914ad6f9f5970ed3bc3c3ac0f51443bcd
maksimmanzhai <bender23max@gmail.com>
2016-03-13 14:33:39 +0300

    access to file

---
+++ AccessToFile/AccessToFile/Source.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+
+using namespace std;
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		cout << "Using: NAME_OF_PROGRAMM <name_of_file> <bit>.\n";
+		return 1;
+	}
+}
